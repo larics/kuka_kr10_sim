e19ee0d2bd4db057a720b81a7bef1b42a1f1fd4d
bmaric <maric.bruno@gmail.com>
2021-12-06 15:32:32 +0100

    Modify publish point

--- kuka_kr10_gazebo/scripts/robotTest.py
+++ kuka_kr10_gazebo/scripts/robotTest.py
@@ -9,67 +9,51 @@ from std_msgs.msg import Empty
 from std_msgs.msg import Header
 
 class PublishPoint():
-	def __init__(self):
-		self.trajPub = rospy.Publisher('/joint_trajectory_action/goal', FollowJointTrajectoryActionGoal, queue_size=10)
+    def __init__(self):
+        self.trajPub = rospy.Publisher('/arm_controller/command', JointTrajectory, queue_size=1)
 
-		self.msg2pub = FollowJointTrajectoryActionGoal()
+    def publish_msg(self, poses2go):
+        # Set the message to publish as command.
+        traj_vector = JointTrajectory()
+        # Current ROS time stamp
+        h = Header()
+        h.stamp = rospy.Time.now()
+        traj_vector.header = h
+        traj_vector.joint_names.append('joint_a1');
+        traj_vector.joint_names.append('joint_a2');
+        traj_vector.joint_names.append('joint_a3');
+        traj_vector.joint_names.append('joint_a4');
+        traj_vector.joint_names.append('joint_a5');
+        traj_vector.joint_names.append('joint_a6');
 
 
-
-		
-
-
-	def publish_msg(self):
-		# Set the message to publish as command.
-		traj_vector = FollowJointTrajectoryActionGoal()
-		# Current ROS time stamp
-		h = Header()
-		h.stamp = rospy.Time.now()
-		traj_vector.header = h
-		traj_vector.goal.trajectory.joint_names.append('joint_a1');
-		traj_vector.goal.trajectory.joint_names.append('joint_a2');
-		traj_vector.goal.trajectory.joint_names.append('joint_a3');
-		traj_vector.goal.trajectory.joint_names.append('joint_a4');
-		traj_vector.goal.trajectory.joint_names.append('joint_a5');
-		traj_vector.goal.trajectory.joint_names.append('joint_a6');
-		traj_vector.goal_id.stamp = h.stamp
-		traj_vector.goal_id.id = 'rosPathPlanner' + str(h.stamp.secs)
-		h2 = Header()
-		h2.stamp.secs = h.stamp.secs #+1
-		h2.stamp.nsecs = h.stamp.nsecs #+ 3e8
-		traj_vector.goal.trajectory.header.stamp = h2.stamp
-
-		points2pub = [[0.0, -0.5, 0.5, 0.5, 0.0, 0.0, 0.0]]
-
-		iter = 0.0
-		for column in points2pub:
-			point = JointTrajectoryPoint()
-			iter +=1
-			for q in column:
-				point.positions.append(q)
-				point.time_from_start.nsecs = 0
-				point.time_from_start.secs = iter*1
-			traj_vector.goal.trajectory.points.append(point)
-	
+        iter = 0.0
+        for column in poses2go:
+            point = JointTrajectoryPoint()
+            iter +=1
+            for q in column:
+                point.positions.append(q)
+                point.time_from_start.nsecs = 0
+                point.time_from_start.secs = int(iter*1)
+            traj_vector.points.append(point)
+    
  
 
-		print traj_vector
-		print 'All systems go!'
-		self.trajPub.publish(traj_vector)	
+        print (traj_vector)
+        print ('All systems go!')
+        self.trajPub.publish(traj_vector)   
 
-	def run(self):
+    def run(self):
 
-		
+        while not rospy.is_shutdown():
 
-		while not rospy.is_shutdown():
-
-			self.publish_msg()
-			print "Running!"
-			rospy.sleep(2)
+            self.publish_msg([[0.0, -0.5, 0.5, 0.5, 0.0, 0.0, 0.0]])
+            print ("Running!")
+            rospy.sleep(3)
 
 
 if __name__ == '__main__':
 
-	rospy.init_node('RobotTest')
-	node = PublishPoint()
-	node.run()
+    rospy.init_node('RobotTest')
+    node = PublishPoint()
+    node.run()
